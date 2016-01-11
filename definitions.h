6db47cd79ed5cda627bcf81e1fc77e84f201c11a
PhilippS92 <philippschlag92@googlemail.com>
2016-01-11 15:35:20 +0100

    basic header file for typedefs, includes etc.

---
+++ definitions.h
@@ -0,0 +1,20 @@
+//
+// Created by philipp on 11.01.16.
+//
+
+#ifndef HASHING_DEFINITIONS_H
+#define HASHING_DEFINITIONS_H
+
+#include <iostream>     // cout, cin
+#include <fstream>      // ifstream, ofstream
+//#include <cstdint>      // uint_fast64_t
+#include <climits>      // ULLONG_MAX
+#include <string>       // string, stoull (to unsigned long long), stod (to double)
+#include <random>       // Mersenne twister mt19937
+#include <functional>   // bind
+using namespace std;
+
+//typedef uint_fast64_t UINT;
+typedef unsigned long long int ULLONG;
+
+#endif //HASHING_DEFINITIONS_H
